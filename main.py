57d6830fa602a4d39f073b45afa9f4782371ccd7
Taiyo <adityanaidu1014@gmail.com>
2025-04-23 22:58:36 +0530

    Batman

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("API_KEY"))
+
+app = Flask(__name__)
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.get_json()
+    prompt = data.get("prompt")
+
+    if not prompt:
+        return jsonify({"error": "Missing 'prompt' in request"}), 400
+    try:
+        response = client.models.generate_content(
+            model="gemini-1.5-flash-8b",
+            contents=prompt,
+        )
+
+        return jsonify({"response": response.text})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
